65cb04b45e5f5dc605fcd3d0a646e2732a7dda43
Vitaliy Shapiro <shapirovet@gmail.com>
2020-02-09 10:22:03 +0300

    hw_2 add TextField

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './Form.css';
 import PropTypes from 'prop-types';
 // import { makeStyles } from '@material-ui/core/styles';
-import { Logo } from 'loft-taxi-mui-theme';
-import { Button, FormLabel, Link, Input, FormHelperText, theme, MuiButton } from '@material-ui/core';
+// import { Logo } from 'loft-taxi-mui-theme';
+import { Grid, Button, Link, /* FormLabel, Input, */ TextField } from '@material-ui/core';
 
 
 class Form extends Component {
@@ -13,6 +13,7 @@ class Form extends Component {
         this.state = {
             login: "",
             password: "",
+            email: "",
             firstName: "",
             lastName: "",
             isReg: true
@@ -44,65 +45,169 @@ class Form extends Component {
     }
 
     render() {
-        const { login, password, firstName, lastName } = this.state;
+        const { login, password, email, firstName, lastName } = this.state;
         
         return (
-            <div className="form-signin">
+            <div className={this.state.isReg ? "form-login" : "form-signup"}>
                 <div className="form-signin-header">{this.state.isReg ? "Войти" : "Регистрация"}</div>
-                <div className="form-signin-text">{this.state.isReg ? "Новый пользователь? " : "Уже зарегистрирован? "}
-                    <a href="/sign" onClick={this.handleReg} >{this.state.isReg ? "Зарегистрироваться" : "Войти"}</a>
+                <div className="form-signin-text">{this.state.isReg ? "Новый пользователь? " : "Уже зарегистрированы? "}
+                    <Link href="/sign" onClick={this.handleReg} >{this.state.isReg ? "Зарегистрируйтесь" : "Войти"}</Link>
                 </div>
 
-                <form onSubmit={this.handleSubmit}>                    
-                    <FormLabel>
-                    Имя пользователя*
-                    <Input
-                        name="login"
-                        type="text"
-                        value={login}
-                        onChange={this.handleChange}
-                    />
-                    </FormLabel>
-                    { !this.state.isReg &&
-                        <>
-                            <label>
-                            Введите своё имя:
-                            <input
-                                name="firstName"
-                                type="text"
-                                value={firstName}
-                                onChange={this.handleChange}
-                            />
-                            </label>
-                            <label>
-                            Введите свою фамилию:
-                            <input
-                                name="lastName"
-                                type="text"
-                                value={lastName}
-                                onChange={this.handleChange}
-                            />
-                            </label>                            
-                        </>                        
-                    }
-                    <label>
-                    Пароль*
-                    <input
-                        name="password"
-                        type="password"
-                        value={password}
-                        onChange={this.handleChange}
-                    />
-                    </label>
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        color="primary"
+                
+                <form onSubmit={this.handleSubmit}>
+                    <Grid container spacing={2}>
+                        { this.state.isReg &&
+                            <>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        // id="standard-multiline-flexible"
+                                        label="Имя пользователя *"
+                                        multiline
+                                        name="login"
+                                        type="text"
+                                        style={{width: "100%" }}
+                                        // error
+                                        // helperText="Неверный логин"
+                                        // rowsMax="4"
+                                        value={login}
+                                        onChange={this.handleChange}
+                                    />
+                                </Grid>
 
-                    >
-                        { this.state.isReg ? "Войти" : "Зарегистрироваться"}
-                    </Button>
-                </form>
+                                {/* <FormLabel>
+                                Имя пользователя *
+                                <Input
+                                    name="login"
+                                    type="text"
+                                    value={login}
+                                    onChange={this.handleChange}
+                                />
+                                </FormLabel> */}
+                            </>
+                        }
+                        { !this.state.isReg &&
+                            <>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        // id="standard-multiline-flexible"
+                                        label="Адрес электронной почты *"
+                                        multiline
+                                        name="email"
+                                        type="email"
+                                        style={{width: "100%" }}
+                                        // error
+                                        // helperText="Неверный логин"
+                                        // rowsMax="4"
+                                        value={email}
+                                        onChange={this.handleChange}
+                                    />
+                                </Grid>
+
+                                {/* <FormLabel>
+                                Адрес электронной почты *
+                                <Input
+                                    name="email"
+                                    type="email"
+                                    value={email}
+                                    onChange={this.handleChange}
+                                />
+                                </FormLabel> */}
+
+                                <Grid container item xs={12} spacing={2}>
+
+                                    <Grid item xs={6}>
+                                        <TextField
+                                            // id="standard-multiline-flexible"
+                                            label="Имя *"
+                                            multiline
+                                            name="firstName"
+                                            type="text"
+                                            style={{ /* marginRight: "16px", */ width: "100%" }}
+                                            // error
+                                            // helperText="Неверный логин"
+                                            // rowsMax="4"
+                                            value={firstName}
+                                            onChange={this.handleChange}
+                                        />
+                                    </Grid>
+
+                                    {/* <FormLabel>
+                                    Имя *
+                                    <Input
+                                        name="firstName"
+                                        type="text"
+                                        value={firstName}
+                                        onChange={this.handleChange}
+                                    />
+                                    </FormLabel> */}
+                                    
+                                    <Grid item xs={6}>
+                                        <TextField
+                                            // id="standard-multiline-flexible"
+                                            label="Фамилия *"
+                                            multiline
+                                            name="lastName"
+                                            type="text"
+                                            style={{/* marginLeft: "16px", */ width: "100%" }}
+                                            // error
+                                            // helperText="Неверный логин"
+                                            // rowsMax="4"
+                                            value={lastName}
+                                            onChange={this.handleChange}
+                                        />
+                                    </Grid>
+
+                                    {/* <FormLabel>
+                                    Фамилия *
+                                    <Input
+                                        name="lastName"
+                                        type="text"
+                                        value={lastName}
+                                        onChange={this.handleChange}
+                                    />
+                                    </FormLabel> */}
+                                </Grid>
+                            </>
+                        }
+
+                        <Grid item xs={12}>
+                            <TextField
+                                // id="standard-multiline-flexible"
+                                label="Пароль *"
+                                multiline
+                                name="password"
+                                type="password"
+                                style={{width: "100%" }}
+                                // error
+                                // helperText="Неверный логин"
+                                // rowsMax="4"
+                                value={password}
+                                onChange={this.handleChange}
+                            />
+                        </Grid>
+
+                        {/* <FormLabel>
+                        Пароль *
+                        <Input
+                            name="password"
+                            type="password"
+                            value={password}
+                            onChange={this.handleChange}
+                        />
+                        </FormLabel> */}
+
+                        <Grid container justify="flex-end" item xs={12}>
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                color="primary"
+                            >
+                                { this.state.isReg ? "Войти" : "Зарегистрироваться"}
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>                
             </div>
 
         );
